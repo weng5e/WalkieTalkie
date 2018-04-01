a534d03517ca47faafdbd136a5163baf5622e98b
Wuyi Weng <wuweng@microsoft.com>
2018-04-01 10:50:16 -0700

    save

---
+++ OxfordSpeechClient/RecognitionSession.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.CognitiveServices.SpeechRecognition;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OxfordSpeechClient
+{
+    public class RecognitionSession : IDisposable
+    {
+        private readonly DataRecognitionClient _client;
+        private TaskCompletionSource<SpeechRecognitionResult> _tcs = new TaskCompletionSource<SpeechRecognitionResult>();
+
+
+        public static async Task<SpeechRecognitionResult> RecognizeSpeechAsync(DataRecognitionClient client, Stream audioStream, CancellationToken token = default(CancellationToken))
+        {
+            using (var session = new RecognitionSession(client))
+            {
+                int bytesRead = 0;
+                byte[] buffer = new byte[1024];
+                // Set the position to the beginning of the stream.
+                audioStream.Seek(0, SeekOrigin.Begin);
+
+                try
+                {
+                    do
+                    {
+                        bytesRead = audioStream.Read(buffer, 0, buffer.Length);
+                        client.SendAudio(buffer, bytesRead);
+                    }
+                    while (bytesRead > 0 && !token.IsCancellationRequested);
+                }
+                finally
+                {
+                    client.EndAudio();
+                    var forgot = Task.Run(async () =>
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(60));
+                        session._tcs.TrySetException(new InvalidOperationException("Timed out."));
+                    });
+                }
+
+                return await session._tcs.Task;
+            }
+        }
+
+        public void Dispose()
+        {
+            _client.OnResponseReceived -= this.OnResponseReceived;
+            _client.OnConversationError -= this.OnConversationError;
+        }
+
+        private void OnResponseReceived(object sender, SpeechResponseEventArgs e)
+        {
+            _tcs.TrySetResult(SpeechRecognitionResult.FromCrisResult(e.PhraseResponse));
+        }
+
+        private void OnConversationError(object sender, SpeechErrorEventArgs e)
+        {
+            _tcs.TrySetException(new InvalidOperationException(e.SpeechErrorText));
+        }
+
+        private RecognitionSession(DataRecognitionClient client)
+        {
+            _client = client;
+            _client.OnResponseReceived += this.OnResponseReceived;
+            _client.OnConversationError += this.OnConversationError;
+        }
+    }
+}
--- OxfordSpeechClient/SpeechRecognitionService.cs
+++ OxfordSpeechClient/SpeechRecognitionService.cs
@@ -11,61 +11,22 @@ namespace OxfordSpeechClient
     public sealed class SpeechRecognitionService : ISpeechRecognitionService
     {
         private readonly CustomSpeechServiceOptions _options;
+        private readonly DataRecognitionClient _client;
 
         public SpeechRecognitionService(CustomSpeechServiceOptions options)
         {
             _options = options;
-
+            _client = SpeechRecognitionServiceFactory.CreateDataClient(SpeechRecognitionMode.ShortPhrase,
+                                                                                 "en-us",
+                                                                                 _options.PrimaryKey,
+                                                                                 _options.SecondaryKey,
+                                                                                 _options.ServiceUrl);
+            _client.AuthenticationUri = _options.AuthenticationUrl;
         }
 
         public async Task<SpeechRecognitionResult> RecognizeSpeechAsync(Stream audioStream, CancellationToken token = default(CancellationToken))
         {
-            using (var client = SpeechRecognitionServiceFactory.CreateDataClient(SpeechRecognitionMode.ShortPhrase,
-                                                                                 "en-us",
-                                                                                 _options.PrimaryKey,
-                                                                                 _options.SecondaryKey,
-                                                                                 _options.ServiceUrl))
-            {
-                client.AuthenticationUri = _options.AuthenticationUrl;
-
-                var tcs = new TaskCompletionSource<SpeechRecognitionResult>();
-
-                client.OnResponseReceived += (sender, e) =>
-                {
-                    tcs.TrySetResult(SpeechRecognitionResult.FromCrisResult(e.PhraseResponse));
-                };
-
-                client.OnConversationError += (sender, e) =>
-                {
-                    tcs.TrySetException(new InvalidOperationException(e.SpeechErrorText));
-                };
-
-                int bytesRead = 0;
-                byte[] buffer = new byte[1024];
-                // Set the position to the beginning of the stream.
-                audioStream.Seek(0, SeekOrigin.Begin);
-
-                try
-                {
-                    do
-                    {
-                        bytesRead = audioStream.Read(buffer, 0, buffer.Length);
-                        client.SendAudio(buffer, bytesRead);
-                    }
-                    while (bytesRead > 0 && !token.IsCancellationRequested);
-                }
-                finally
-                {
-                    client.EndAudio();
-                    var forgot = Task.Run(async () =>
-                    {
-                        await Task.Delay(TimeSpan.FromSeconds(60));
-                        tcs.TrySetException(new InvalidOperationException("Timed out."));
-                    });
-                }
-
-                return await tcs.Task;
-            }
+            return await RecognitionSession.RecognizeSpeechAsync(_client, audioStream, token);
         }
 
         public void Dispose()
