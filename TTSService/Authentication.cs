01e7aa2a47562cb5a2b3d0b9be33cf2d6c70d22d
Wuyi Weng <wuweng@microsoft.com>
2018-04-02 10:17:28 -0700

    - add new client library place holder.

---
+++ TTSService/Authentication.cs
@@ -0,0 +1,135 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+//
+// Microsoft Cognitive Services (formerly Project Oxford): https://www.microsoft.com/cognitive-services
+//
+// Microsoft Cognitive Services (formerly Project Oxford) GitHub:
+// https://github.com/Microsoft/Cognitive-Speech-TTS
+//
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+//
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace TTSService
+{
+    public class Authentication
+    {
+        private readonly TTSServiceOptions _options;
+        private string accessToken;
+        private Timer accessTokenRenewer;
+
+        //Access token expires every 10 minutes. Renew it every 9 minutes only.
+        private const int RefreshTokenDuration = 9;
+
+        public Authentication(TTSServiceOptions options)
+        {
+            _options = options;
+
+            this.accessToken = HttpPost(_options.AuthenticationUri, _options.ApiKey);
+
+            // renew the token every specfied minutes
+            accessTokenRenewer = new Timer(new TimerCallback(OnTokenExpiredCallback),
+                                           this,
+                                           TimeSpan.FromMinutes(RefreshTokenDuration),
+                                           TimeSpan.FromMilliseconds(-1));
+        }
+
+        public string GetAccessToken()
+        {
+            return this.accessToken;
+        }
+
+        private void RenewAccessToken()
+        {
+            var newAccessToken = HttpPost(_options.AuthenticationUri, _options.ApiKey);
+            //swap the new token with old one
+            //Note: the swap is thread unsafe
+            this.accessToken = newAccessToken;
+            Console.WriteLine(string.Format("Renewed token for user: {0} is: {1}",
+                              this._options.ApiKey,
+                              this.accessToken));
+        }
+
+        private void OnTokenExpiredCallback(object stateInfo)
+        {
+            try
+            {
+                RenewAccessToken();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Failed renewing access token. Details: {0}", ex.Message));
+            }
+            finally
+            {
+                try
+                {
+                    accessTokenRenewer.Change(TimeSpan.FromMinutes(RefreshTokenDuration), TimeSpan.FromMilliseconds(-1));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Format("Failed to reschedule the timer to renew access token. Details: {0}", ex.Message));
+                }
+            }
+        }
+
+        private string HttpPost(string accessUri, string apiKey)
+        {
+            // Prepare OAuth request
+            WebRequest webRequest = WebRequest.Create(accessUri);
+            webRequest.Method = "POST";
+            webRequest.ContentLength = 0;
+            webRequest.Headers["Ocp-Apim-Subscription-Key"] = apiKey;
+
+            using (WebResponse webResponse = webRequest.GetResponse())
+            {
+                using (Stream stream = webResponse.GetResponseStream())
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        byte[] waveBytes = null;
+                        int count = 0;
+                        do
+                        {
+                            byte[] buf = new byte[1024];
+                            count = stream.Read(buf, 0, 1024);
+                            ms.Write(buf, 0, count);
+                        } while (stream.CanRead && count > 0);
+
+                        waveBytes = ms.ToArray();
+
+                        return Encoding.UTF8.GetString(waveBytes);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TTSService/TTSServiceOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TTSService
+{
+    public class TTSServiceOptions
+    {
+        public string AuthenticationUri { get; set; } = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+
+        public string ApiKey { get; set; }
+    }
+}
